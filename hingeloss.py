a76f26bef27155b83e3cfc916f7033df6cb6b9e4
LizardFeng <52057417+chaoyufeng93@users.noreply.github.com>
2022-03-28 15:46:34 +0900

    Update hingeloss.py

--- hingeloss.py
+++ hingeloss.py
@@ -3,6 +3,9 @@ class MyHingeLoss(torch.nn.Module):
     super(MyHingeLoss, self).__init__()
     self.max_z = torch.nn.ReLU()
  
-  def forward(self, asp, emb, y):
-    out = -1 * self.max_z(1 - (asp*(y*emb)).sum(axis = -1))
+  def forward(self, asp, emb, y, reg_turn):
+
+    y = y.reshape(-1,1).repeat(1,emb.shape[-1])
+    out = self.max_z(1 - (asp*(y*emb)).sum(axis = -1)) + reg_turn
+
     return out
